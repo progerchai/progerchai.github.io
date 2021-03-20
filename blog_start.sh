40137e919a75d9fdf86cce4a7c1f13fee5981174
proger <progerchai@qq.com>
2021-03-20 15:37:55 +0800

    add blog start shell command

---
+++ blog_start.sh
@@ -0,0 +1,16 @@
+# 路径
+BLOGPATH='.'
+
+cd $BLOGPATH
+echo '开始拉取代码>>>>>>>>>>>>>>>>'
+git pull
+echo '拉取代码完成>>>>>>>>>>>>>>>>'
+cd $BLOGPATH/site/
+
+echo '开始搭建>>>>>>>>>>>>>>>>'
+npm install && npm run build
+echo '开始部署>>>>>>>>>>>>>>>>'
+npx hexo deploy --generate
+# === hexo d -g
+
+echo '运行完成>>>>>>>>>>>>>>>>'
