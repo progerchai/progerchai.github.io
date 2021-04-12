bdb4b54d2b037ca1c4436a48fafaad408c63dcbc
proger <progerchai@qq.com>
2021-04-12 21:49:32 +0800

    update blog_start.sh

--- blog_start.sh
+++ blog_start.sh
@@ -1,15 +1,24 @@
 # 博客部署根目录
 BLOGROOTPATH='/www/wwwroot/www.proger.cn/'
 
-cd $BLOGROOTPATH
-echo '清除代码>>>>>>>>>>>>>>>>'
-# 删除代码
-rm -rf progerchai.github.io
 
-echo '开始拉取代码>>>>>>>>>>>>>>>>'
+cd $BLOGROOTPATH
+
 # git clone https://github.com/progerchai/progerchai.github.io.git
 
-git clone https://github.com.cnpmjs.org/progerchai/progerchai.github.io.git
+# git clone https://github.com.cnpmjs.org/progerchai/progerchai.github.io.git
+
+# 判断是否已存在代码,若已存在则只需要pull即可
+echo '开始拉取代码>>>>>>>>>>>>>>>>'
+if [ ! -d "/progerchai.github.io/" ];then
+  echo '清除代码>>>>>>>>>>>>>>>>'
+  git clone https://github.com.cnpmjs.org/progerchai/progerchai.github.io.git
+  else
+  git pull
+fi
+
+# 删除代码
+# rm -rf progerchai.github.io
 
 echo '拉取代码完成>>>>>>>>>>>>>>>>'
 cd $BLOGROOTPATH/progerchai.github.io/site/
@@ -17,10 +26,8 @@ cd $BLOGROOTPATH/progerchai.github.io/site/
 echo '开始搭建>>>>>>>>>>>>>>>>'
 npm install
 echo '开始部署>>>>>>>>>>>>>>>>'
-npx hexo deploy --generate
+npx hexo deploy --generate && npx gulp
 
-echo '正在进行资源压缩>>>>>>>>>>>>>>>>'
-npx gulp
 # === hexo d -g
 
 echo '运行完成>>>>>>>>>>>>>>>>'
